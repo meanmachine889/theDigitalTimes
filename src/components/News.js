1f8903d737a548369736eb12dc9f3a420ce5edaa
Yash Bharadwaj <116567556+meanmachine889@users.noreply.github.com>
2024-05-10 22:50:33 +0530

    Update News.js

--- src/components/News.js
+++ src/components/News.js
@@ -17,7 +17,7 @@ const News = (props) => {
 
   const updateNews = async () => {
     props.setProgress(10);
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=24c7db8b67e84a34a25f47ad6ee940ff&page=${page}&pageSize=${props.pageSize}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=9d06445f8fb54c35ada9190dc3be9205&page=${page}&pageSize=${props.pageSize}`;
     setLoading(true);
     let data = await fetch(url);
     props.setProgress(30);
@@ -38,7 +38,7 @@ const News = (props) => {
   const fetchMoreData = async () => {
     const url = `https://newsapi.org/v2/top-headlines?country=${
       props.country
-    }&category=${props.category}&apiKey=24c7db8b67e84a34a25f47ad6ee940ff&page=${
+    }&category=${props.category}&apiKey=9d06445f8fb54c35ada9190dc3be9205&page=${
       page + 1
     }&pageSize=${props.pageSize}`;
     setPage(page + 1);
